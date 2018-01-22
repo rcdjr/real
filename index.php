51d43796abc6f54a5cc9df0284b8013efdcec585
Ricardo Oliveira Junior <rcdjr@Ricardos-MacBook-Pro.local>
2018-01-22 02:12:25 -0200

    redirect test

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 include_once("index.html");
 
-// header("HTTP/1.1 301 Moved Permanently");
-// header("Location: ");
-// exit();
+header("HTTP/1.1 301 Moved Permanently");
+header("Location: http://contrealteste.pessoal.ws/one-page/index.html ");
+exit();
 
 ?>
